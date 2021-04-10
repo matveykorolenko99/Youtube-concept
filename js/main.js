4d711b1522de542431c3757fee02f7a0052c770a
matveykorolenko99 <79077816+matveykorolenko99@users.noreply.github.com>
2021-04-10 23:13:16 +0300

    add mobile adaptation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,105 @@
-const swiper = new Swiper('.channel-slider ', {
+const swiper = new Swiper('.channel-slider#first ', {
     // Optional parameters
 
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+        1900: {
+            slidesPerView: 6
+
+        },
+        1600: {
+            slidesPerView: 5
+        },
+        1300: {
+            slidesPerView: 4
+        },
+        1100: {
+            slidesPerView: 5
+        },
+        750: {
+            slidesPerView: 2
+        }
+    },
 
 
     // Navigation arrows
     navigation: {
-        nextEl: '.channel-button-next',
-        prevEl: '.channel-button-prev',
+        nextEl: '.channel-button-next1',
+
+        prevEl: '.channel-button-prev1',
+
     },
 
 
+
+});
+
+const swipers = new Swiper('.channel-slider#second ', {
+    // Optional parameters
+
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+        1500: {
+            slidesPerView: 3
+
+        },
+
+
+        900: {
+            slidesPerView: 2
+        }
+
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.channel-button-next2',
+
+        prevEl: '.channel-button-prev2',
+
+    },
+});
+
+const swiper3 = new Swiper('.channel-slider#third ', {
+    // Optional parameters
+
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+        1900: {
+            slidesPerView: 6
+
+        },
+        1600: {
+            slidesPerView: 5
+        },
+        1300: {
+            slidesPerView: 4
+        },
+        1100: {
+            slidesPerView: 5
+        },
+        750: {
+            slidesPerView: 2
+        }
+    },
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.channel-button-next3',
+
+        prevEl: '.channel-button-prev3',
+
+    }
+
+});
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+    mobileSearch.classList.toggle('is-open')
 });
